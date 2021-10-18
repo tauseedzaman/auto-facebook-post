2cfc102d2210ec25da63bc427e7bafcf76b755b9
tauseed zaman <tauseedzamanofsorana@gmail.com>
2021-10-18 21:08:49 +0500

    dailypakistan added

--- auto-fb.py
+++ auto-fb.py
@@ -15,6 +15,18 @@ import requests
 
 #getting news from different pakistani's websites
 
+# get latest news from DailyPakistan
+def get_DailyPakistan_News():
+    html_text=requests.get("https://en.dailypakistan.com.pk/latest").text
+    soupp = soup(html_text, 'lxml')
+    main_container = soupp.find_all("div",{'class':["col-xs-12 col-sm-4 col-lg-3 verticle-widget-col news3"]})
+    output = "Top Stories on DailyPakistan\n"
+    for i,item in enumerate(main_container):
+        heading = item.find("small").text
+        link = item.find("a").get("href")
+        output = output + (f"No: {i} \n title: {heading} \n link: {link} \n\n")
+    return output
+
 #get news from thenews.com.pk
 def get_TheNews_News():
     html_text=requests.get("https://www.thenews.com.pk/latest-stories").text
@@ -34,12 +46,10 @@ def get_Geo_News():
     html_text=requests.get("https://www.geo.tv/").text
     soupp = soup(html_text, 'lxml')
     main_container = soupp.find_all("article")
-    i=1
     output = "Top Stories on GEO News\n"
-    for item in main_container:
+    for i,item in enumerate(main_container):
         a_tage = item.find("a")
         output = output + (f"No:[{i}] title: {a_tage.text.strip()} \n link: {a_tage.get('href')} \n\n")
-        i=i+1
     return output
 
 #fetching hashtags for facebook post
@@ -65,7 +75,6 @@ def hashtags(hash_idea):
 
 # login to facebook account
 def login(username, password):
-
     try:
         url = 'https://facebook.com'
         driver.get(url)
@@ -80,31 +89,39 @@ def login(username, password):
         print('Something went wrong while login process')
 
 
-def post(caption):
+
+def post(caption="Hello World"):
     try:
-        add = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[1]/div[3]/div[2]/span/div/i')))
-        add.click()
 
-        click_add_post = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div[1]/span')))
-        click_add_post.click()
+            # click on + button
+            add = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[1]/div[3]/div[2]/span/div/i')))
+            add.click()
+            # click on post
+            click_add_post = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div[1]/span')))
+            click_add_post.click()
 
-        sleep(5)
-        print(caption)
-        cap = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[4]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div/div/div')))
-        cap.send_keys(caption)
-        
-        sleep(5) # this is mandatory while doing some thing with bot
-        btn_post = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[4]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/div/span/span')))
-        btn_post.click()
+            sleep(5)
+            print(caption)
+            # select text box
+            # /html/body/div[11]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div/div/div
+            cap = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[11]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div/div/div')))
+            cap.send_keys(caption)
+            
+            sleep(5) # this is mandatory while doing some thing with bot
+            # /html/body/div[11]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/div/span/span
+            btn_post = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[11]/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/div/span/span')))
+            btn_post.click()
     except:
-        print('Something Went Wrong While posting ')
+            print('Something Went Wrong While posting ')
+
 if __name__== "__main__":
     #turn for credentials, driver, and caption
     username = "" #input('ENTER USERNAME : ')
     password = "" #input('ENTER PASSWORD : ')
     img_path = "" #input('Enter Image Path : ')
-    hash_idea = "" #input('ENTER ONE HASH : ')
-    caption = str(get_Geo_News().strip()) + '\n\n' + hashtags(hash_idea)
-    driver = webdriver.Chrome("path")
+    hash_idea = "PakistanNews" #input('ENTER ONE HASH : ')
+    caption = ""  
+ #str(get_Geo_News().strip()) + '\n\n' + hashtags(hash_idea)
+    driver = webdriver.Chrome("")
     login(username,password)
-    post(caption)
+    post(get_DailyPakistan_News())
